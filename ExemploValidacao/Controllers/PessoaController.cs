6623607760f95064d0cd0f3e9f67cc4bcf44ba97
Rafael Carlos <shanglangjunior@hotmail.com>
2015-06-21 16:51:38 -0300

    Iniciando com ASP.NET
    Implementação de um simples exemplo de validadeção envio de formulário,
    em ASP.NET.

---
+++ ExemploValidacao/Controllers/PessoaController.cs
@@ -0,0 +1,63 @@
+﻿using ExemploValidacao.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExemploValidacao.Controllers
+{
+    public class PessoaController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pessoa pessoa)
+        {
+            //Validação dos campos nome e senha.
+            //if(string.IsNullOrEmpty(pessoa.Nome))
+            //{
+            //    ModelState.AddModelError("Nome", "Preencha o nome campo");
+            //}
+
+            //if(pessoa.Senha != pessoa.ConfirmarSenha)
+            //{
+            //    ModelState.AddModelError("", "Senhas não conferem");
+            //}
+
+
+
+            if(ModelState.IsValid)
+            {
+                return View("Resultado", pessoa);
+            }
+            return View(pessoa);
+        }
+
+        public ActionResult Resultado(Pessoa pessoa)
+        {
+            return View(pessoa);
+        }
+
+        public ActionResult LoginUnico(string login)
+        {
+            var bancoExemplo = new Collection<String>
+            {
+                "Rafael",
+                 "Sequela",
+                 "Radhar"
+            };
+
+            //Para cada registro, trasforme tudo para minusculo
+            return Json(bancoExemplo.All(x => x.ToLower() != login.ToLower()) , JsonRequestBehavior.AllowGet); 
+        }
+
+    }
+}
---
+++ ExemploValidacao/Models/Pessoa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ExemploValidacao.Models
+{
+    public class Pessoa
+    {
+        [Required(ErrorMessage = "O nome deve ser preenchido")]
+        public string Nome { get; set; }
+
+        [StringLength (50, MinimumLength= 5, ErrorMessage="A observação deve ter entre 5 e 50 caracteres")]
+        [Required(ErrorMessage="Preencha a Observação")]
+        public string Observacao { get; set; }
+
+        [Range(18, 50, ErrorMessage= "Idade deve ser entre 18 e 50 anos")]
+        public int Idade { get; set; }
+
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage="Insira um email válido")]
+        public string Email { get; set; }
+
+        [RegularExpression (@"[a-zA-Z]{5,15}", ErrorMessage="O nome deve apenas letras")]
+        [Required(ErrorMessage="O login deve ser informado")]
+        public string Login { get; set; }
+
+        [Required(ErrorMessage="A senha deve ser informada")]
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage="Senhas não conferem")]
+        public string ConfirmarSenha { get; set; }
+    }
+}
